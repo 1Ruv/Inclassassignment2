61d94b4d51763dc5869cb2f5a783c472e367cf65
1Ruv <zehaobrianhe@gmail.com>
2023-10-24 14:28:55 -0400

    final

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggleImage(objectID) {
+  document.getElementById(objectID).style.display = "block";
+}
+
+function hideImage(objectID) {
+  document.getElementById(objectID).style.display = "none";
+}
